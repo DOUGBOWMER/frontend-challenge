af48ff5090dd1cadd0c4acd303aba052c9b55cfa
DOUGBOWMER <dougbowmer3@gmail.com>
2021-12-01 22:32:07 -0500

    style: Structure layout of slider card
    I have began The process of creating the slider card.
    This is usually when I would layout any data prop
    that I may need to help visualize the layout and how the data fits.

--- slider/responsive-slider/components/Slider.js
+++ slider/responsive-slider/components/Slider.js
@@ -1,5 +1,45 @@
 import React from 'react'
 
+
+const data = [
+    {
+        id: 1,
+        title:'Gradients',
+        subTitle: "start. end, angle",
+        from: "from-puple-900",
+        via:"via-blue-500",
+        to:-'to-green-200',
+        icon: "gradients.svg"
+    },
+    {
+        id: 2,
+        title:'Presets',
+        subTitle: "Manage presets",
+        from: "from-green-200",
+        via:"via-green-100",
+        to:-'to-yellow-200',
+        icon: "presets.svg"
+    },
+    {
+        id: 3,
+        title:'Colors',
+        subTitle: "Pick any color",
+        from: "from-yellow-200",
+        via:"via-oroange-200",
+        to:-'to-pink-200',
+        icon: "colors.svg"   
+    },
+]; 
+
+
+
+
+
+
+
+
+
+
 function Slider() {
     return (
         <div>
