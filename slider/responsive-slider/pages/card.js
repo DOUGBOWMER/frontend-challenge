5f820d7613e7992a88904b884babff9bf7b560f8
DOUGBOWMER <dougbowmer3@gmail.com>
2021-12-01 19:59:19 -0500

    chore: Build Next.js package include tailwind CSS
    Preset known objects to begin intital layout process.
    Responsive slider will be composed
    of a card and a secondary compnent the slider.
     I will attempt to use state to animate the slider upon click.

---
+++ slider/responsive-slider/pages/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../components/Card'
+
+function card() {
+    return (
+        <div>
+            <Card/>
+        </div>
+    )
+}
+
+export default card
---
+++ slider/responsive-slider/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        green: {
+          200:'#02eff7',
+          100:'#92f394'
+        },
+        yellow: {
+          200: '#ffec0c'
+        },
+        pink: {
+          200: '#fd0a76'
+        },
+        orange: {
+          200: "#ff775a"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
